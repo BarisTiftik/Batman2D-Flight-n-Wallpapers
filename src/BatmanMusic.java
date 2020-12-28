c711560f4ecf31ae790c30547ab8cf8433be068f
barış tiftik <brstftk@hotmail.com>
2020-12-28 03:23:22 +0300

    Music bug fixed. It appears v1.0 will be deleted soon.

--- src/BatmanMusic.java
+++ src/BatmanMusic.java
@@ -1,13 +1,11 @@
 import sun.audio.AudioPlayer;
 import sun.audio.AudioStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 public class BatmanMusic {
     // methods
     public void playMusic() throws IOException {
-        FileInputStream music = new FileInputStream("C:/Users/TULPAR/IdeaProjects/Batman2D-Flight-&-Wallpapers/res/TheDarkKnightRises(MainTheme).wav");
-        AudioStream audioStream = new AudioStream(music);
+        AudioStream audioStream = new AudioStream(getClass().getClassLoader().getResourceAsStream("TheDarkKnightRises(MainTheme).wav"));
         AudioPlayer.player.start(audioStream);
     }
 }
